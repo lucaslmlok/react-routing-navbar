f93d5cd9a1c27a2344317b56702610bb2b18af4e
Lucas Lo <lucaslo@creasant.com>
2022-06-01 09:15:41 -0400

    react routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import About from "./pages/About";
+
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
